add84fa502bae690cde4f50759e6ffbc836b4bfb
kaeru2193 <kaeru2193@gmail.com>
2024-07-20 18:38:07 +0900

    fixed: api build error.

--- app/api/phun-clock-svg/route.tsx
+++ app/api/phun-clock-svg/route.tsx
@@ -1,17 +1,6 @@
 import { ClockSVG } from './svg';
 import { getClockParams } from './clockCalc';
 
-export const SvgExample = () => {
-    return <svg
-      version="1.1"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect x="0" y="0" width="100" height="100" fill="red" />
-      <rect x="20" y="40" width="100" height="100" fill="blue" />
-      <rect x="30" y="20" width="100" height="100" fill="green" />
-    </svg>
-}
-
 export function GET() {
     const ReactDOMServer = require('react-dom/server')
     const p = getClockParams()
