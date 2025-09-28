c2cb435b78acda59295cd728f49dbdc30cffb9d2
kaeru2193 <kaeru2193@gmail.com>
2025-09-28 14:33:03 +0900

    fixed: bugs.

--- app/clock-widget/page.tsx
+++ app/clock-widget/page.tsx
@@ -1,10 +1,14 @@
 "use client"
 
+import style from "./page.module.css"
 import { PhunClockWidget } from "@/common"
 
 const Page = () => {
     return (
-        <PhunClockWidget/>
+        <>
+            <span className={style.title}><h1>配信用雰時計</h1></span>
+            <PhunClockWidget/>
+        </>
     )
 }
 
